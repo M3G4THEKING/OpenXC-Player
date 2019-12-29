45baf9855b841167b4e30dc3637c1e30e8cb6c07
flippz <flippy@flippy.no>
2019-12-29 03:51:55 +0100

    Update header.php
    Layout changes
    Language fix

--- inc/header.php
+++ inc/header.php
@@ -1,5 +1,5 @@
 <div class="col-lg-9 col-md-8 col-sm-12 col-xs-12">
-               <a class="main-logo" href="painel.php?sessao=<?php echo gerar_hash(256); ?>"><img style="height:140px;" src="assets/<?php echo $template; ?>/img/logo-player.png" class="main-logo img-responsive" alt="<?php echo NOME_IPTV; ?>" title="<?php echo NOME_IPTV; ?>"></a>
+               <a class="main-logo" href="painel.php?sessao=<?php echo gerar_hash(256); ?>"><img style="height:80px;" src="assets/<?php echo $template; ?>/img/logo-player.png" class="main-logo img-responsive" alt="<?php echo NOME_IPTV; ?>" title="<?php echo NOME_IPTV; ?>"></a>
             </div>
            
             <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12">
@@ -11,6 +11,6 @@
             	$data_inicio = new DateTime(date('Y-m-d'));
     			$data_fim = new DateTime($dsfim);
     			$dateInterval = $data_inicio->diff($data_fim);
-    			echo $dateInterval->days . ' Dias';
+    			echo $dateInterval->days . ' Days';
     			?>
             </div>
